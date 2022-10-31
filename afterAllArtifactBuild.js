916b6410de907722a48fc1e7f117916110397280
Nathan Faucher <nathan.j.faucher@gmail.com>
2022-10-30 23:02:29 -0600

    Run beforePack and afterBuild hooks when calling `start`

--- afterAllArtifactBuild.js
+++ afterAllArtifactBuild.js
@@ -1,7 +1,12 @@
-module.exports = () => {
+const main = () => {
     const fs = require('fs');
     // Return project folder state to before build
     fs.unlinkSync('icon.png');
     fs.unlinkSync('actual-server/load-config.js');
     fs.renameSync('actual-server/_load-config.js', 'actual-server/load-config.js');
+}
+
+module.exports = main
+if (typeof require !== 'undefined' && require.main === module) {
+    main();
 }
--- beforePack.js
+++ beforePack.js
@@ -1,6 +1,6 @@
 const { config } = require('process');
 
-module.exports = () => {
+const main = () => {
     const fs = require('fs');
     // Copy the current icon from `@actual-app` to be used as the app icon
     fs.copyFileSync('node_modules/@actual-app/web/build/android-chrome-512x512.png', 'icon.png');
@@ -8,4 +8,9 @@ module.exports = () => {
     fs.renameSync('actual-server/load-config.js', 'actual-server/_load-config.js')
     // Copy `load-config.js` from `actual-desktop` into `actual-server`
     fs.copyFileSync('load-config.js', 'actual-server/load-config.js');
+}
+
+module.exports = main
+if (typeof require !== 'undefined' && require.main === module) {
+    main();
 }
